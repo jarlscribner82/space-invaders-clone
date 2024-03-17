743fab27dd9ca5fa59e8b9c0a244f30c49cc8e97
jarlscribner82 <142836988+jarlscribner82@users.noreply.github.com>
2024-03-17 18:14:01 -0400

    Spawn manager code cleaned and commented

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    // supporter references
     [SerializeField] GameObject support;
     [SerializeField] List<Transform> emptySupportSpawnPoints = new List<Transform>();
     private List<Transform> fullSupportSpawnPoints = new List<Transform>();
 
+    // tank references
     [SerializeField] GameObject tank;
     [SerializeField] List<Transform> emptyTankSpawnPoints = new List<Transform>();
     private List<Transform> fullTankSpawnPoints = new List<Transform>();
 
+    // infantry references
     [SerializeField] GameObject infantry;
     [SerializeField] List<Transform> emptyInfantrySpawnPoints = new List<Transform>();
     private List<Transform> fullInfantrySpawnPoints = new List<Transform>();
 
+    // ranged references
     [SerializeField] GameObject ranged;
     [SerializeField] List<Transform> emptyRangedSpawnPoints = new List<Transform>();
     private List<Transform> fullRangedSpawnPoints = new List<Transform>();
@@ -31,11 +35,10 @@ public class SpawnManager : MonoBehaviour
         if (testSpawn)
         {
             SpawnSupporter();
-            SpawnInfantry();
-            SpawnRanged();
-            SpawnTank();
         }
     }
+
+    // spawn a supporter in a random supporter spawn point as long there are empty points
     void SpawnSupporter()
     {
         if (emptySupportSpawnPoints.Count > 0)
@@ -43,9 +46,8 @@ public class SpawnManager : MonoBehaviour
             int randomIndex = Random.Range(0, emptySupportSpawnPoints.Count);
             var selectedPoint = emptySupportSpawnPoints[randomIndex];
 
-            if (selectedPoint != null && emptySupportSpawnPoints.Count > 0)
+            if (selectedPoint != null)
             {
-
                 Instantiate(support, selectedPoint);
                 fullSupportSpawnPoints.Add(emptySupportSpawnPoints[randomIndex]);
                 emptySupportSpawnPoints.RemoveAt(randomIndex);
@@ -53,6 +55,7 @@ public class SpawnManager : MonoBehaviour
         }        
     }
 
+    // spawn a tank in a random tank spawn point as long there are empty points
     void SpawnTank()
     {
         if (emptyTankSpawnPoints.Count > 0)
@@ -60,9 +63,8 @@ public class SpawnManager : MonoBehaviour
             int randomIndex = Random.Range(0, emptyTankSpawnPoints.Count);
             var selectedPoint = emptyTankSpawnPoints[randomIndex];
 
-            if (selectedPoint != null && emptyTankSpawnPoints.Count > 0)
+            if (selectedPoint != null)
             {
-
                 Instantiate(tank, selectedPoint);
                 fullTankSpawnPoints.Add(emptyTankSpawnPoints[randomIndex]);
                 emptyTankSpawnPoints.RemoveAt(randomIndex);
@@ -70,6 +72,7 @@ public class SpawnManager : MonoBehaviour
         }
     }
 
+    // spawn an infantry in a random infantry spawn point as long there are empty points
     void SpawnInfantry()
     {
         if (emptyInfantrySpawnPoints.Count > 0)
@@ -77,16 +80,15 @@ public class SpawnManager : MonoBehaviour
             int randomIndex = Random.Range(0, emptyInfantrySpawnPoints.Count);
             var selectedPoint = emptyInfantrySpawnPoints[randomIndex];
 
-            if (selectedPoint != null && emptyInfantrySpawnPoints.Count > 0)
+            if (selectedPoint != null)
             {
-
                 Instantiate(infantry, selectedPoint);
                 fullInfantrySpawnPoints.Add(emptyInfantrySpawnPoints[randomIndex]);
                 emptyInfantrySpawnPoints.RemoveAt(randomIndex);
             }
         }
     }
-
+    // spawn a ranged in a random ranged spawn point as long there are empty points
     void SpawnRanged()
     {
         if (emptyRangedSpawnPoints.Count > 0)
@@ -94,9 +96,8 @@ public class SpawnManager : MonoBehaviour
             int randomIndex = Random.Range(0, emptyRangedSpawnPoints.Count);
             var selectedPoint = emptyRangedSpawnPoints[randomIndex];
 
-            if (selectedPoint != null && emptyRangedSpawnPoints.Count > 0)
+            if (selectedPoint != null)
             {
-
                 Instantiate(ranged, selectedPoint);
                 fullRangedSpawnPoints.Add(emptyRangedSpawnPoints[randomIndex]);
                 emptyRangedSpawnPoints.RemoveAt(randomIndex);
