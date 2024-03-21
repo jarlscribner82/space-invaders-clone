11d6b3388172b638443653701d79bd576d1dbff9
jarlscribner82 <142836988+jarlscribner82@users.noreply.github.com>
2024-03-20 21:57:13 -0400

    better overload example using infntry units
    -infantry
        - movement with ai added
            - uses the MoveTo overloaded method using a rigid body or transform as  a target to move to
            - defensive post prefabs were created and assigned to the unit prefabs to enable random selection from an array
           - randomly assigns a defensive post in awake and uses an extra collider to sense if  the player is shielding on approach
         - offensive state toggles based on a coroutine timer
              - eliminates kamikaze behavior and allows for a large force to accumulate for a massive attack

--- Assets/Scripts/InfantryMobController.cs
+++ Assets/Scripts/InfantryMobController.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class InfantryMobController : MobController
 {
+    // player reference
+    private PlayerController player;
+
     // allow access to rigid body
     private Rigidbody infantryRb;
+
+    // attack state
+    [SerializeField] bool offensive = true;
+
+    // post references
+    [SerializeField] Transform[] posts;
+    private Transform post;
     
     // attributes unique to an infantry mob
     [SerializeField] float m_Speed;
@@ -24,6 +35,10 @@ public class InfantryMobController : MobController
 
         // allow access to rigid body
         infantryRb = GetComponent<Rigidbody>();
+
+        post = AssignPost();
+
+        player = GameObject.Find("player").GetComponent<PlayerController>();
     }
 
     protected override void Start()
@@ -34,14 +49,28 @@ public class InfantryMobController : MobController
     // Update is called once per frame
     protected override void Update()
     {
-        FollowPlayer();
+        MoveWithAI();
+    }
+
+    // infantry movement with simple AI determining offensive or defensive movement
+    void MoveWithAI()
+    {
+        if (offensive)
+        {
+            MovetTo(player.playerRb);
+        }
+        else
+        {
+            MovetTo(post);
+            StartCoroutine(Cooldown());
+        }
     }
 
     // enable enemy track and follow for player
-    void FollowPlayer()
-    {
+    void MovetTo(Rigidbody target)
+    { 
         // get the vector that points in the players direction
-        Vector3 getDirection = (playerController.playerRb.transform.position - infantryRb.transform.position).normalized;
+        Vector3 getDirection = (target.transform.position - infantryRb.transform.position).normalized;
 
         // move the enemy towards the player
         infantryRb.AddForce(getDirection * Speed, ForceMode.Impulse);
@@ -55,6 +84,34 @@ public class InfantryMobController : MobController
         KeepInBounds();
     }
 
+    // enable enemy to seek a specific location
+    void MovetTo(Transform target)
+    {
+        // get the vector that points in the players direction
+        Vector3 getDirection = (target.transform.position - infantryRb.transform.position).normalized;
+
+        // move the enemy towards the player
+        infantryRb.AddForce(getDirection * Speed, ForceMode.Impulse);
+
+        // set a max speed
+        if (infantryRb.velocity.magnitude > Speed)
+        {
+            infantryRb.velocity = infantryRb.velocity.normalized * Speed;
+        }
+
+        KeepInBounds();
+    }
+
+    // assign a random defensive post for unit to flee to when defensive
+    Transform AssignPost()
+    {
+        int postNumber = Random.Range(0, posts.Length);
+        
+        post = posts[postNumber];
+
+        return post;
+    }
+
     // keep the mpob in the game field
     void KeepInBounds()
     {
@@ -66,6 +123,13 @@ public class InfantryMobController : MobController
         {
             infantryRb.transform.position = new Vector3(boundaryRange, infantryRb.transform.position.y, infantryRb.transform.position.z);
         }
+    }
+
+    // action cooldown timer
+    private IEnumerator Cooldown()
+    {
+        yield return new WaitForSecondsRealtime(cooldownMax);
+        offensive = true;
     }
 
     // raise speed
@@ -84,6 +148,15 @@ public class InfantryMobController : MobController
             DealDamage();
             SpawnManager.instance.enemyCount--;
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // change to defense mode when in range of the shielded player
+        if (other.gameObject.CompareTag("Player") && player.isShielding)
+        {
+            offensive = false;
         }
     }
 }
